7303939fe21c4b2bcfbed85994db91a337825e27
zhengchao <chao.zheng@welian.com>
2019-01-15 15:15:02 +0800

    讲笑话程序

---
+++ Pygame_project/chapter4/speak_joke.py
@@ -0,0 +1,21 @@
+# @Time    : 2019/1/15 下午3:02
+# @Author  : 郑超
+# @Desc    : 讲笑话程序
+print("What do you get when you cross a snowman with a vampire?")
+input()
+print("Frostbite!")
+print("-"*80)
+
+print("What do dentists call an astronaut's cavity")
+input()
+print("A black hole")
+print("-"*80)
+
+print("Knock Knock")
+input()
+print("Who's there")
+print("-"*80)
+
+print("Interrupting cow.")
+input()
+print("Interrupting cow wh\n-MOO!")
